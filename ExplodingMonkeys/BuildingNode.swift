41a4df785532edd35d7e6e5becf6fbf6cc47305c
Paul Richardson <developer@photographerafoot.com>
2021-06-19 08:58:29 +1000

    CHG: set up building

--- ExplodingMonkeys/BuildingNode.swift
+++ ExplodingMonkeys/BuildingNode.swift
@@ -10,4 +10,22 @@ import UIKit
 
 class BuildingNode: SKSpriteNode {
 
+	var currentImage: UIImage!
+
+	func setup() {
+		name = "building"
+		currentImage = drawBuilding(size: size)
+		texture = SKTexture(image: currentImage)
+
+		configurePhysics()
+	}
+
+	configurePhysics() {
+
+	}
+
+	drwaBuilding(size: CGSize) -> UIImage {
+
+	}
+
 }

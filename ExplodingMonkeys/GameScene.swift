0a3a993b3d64b3f1154fed65b0c6587da02401ca
Paul Richardson <developer@photographerafoot.com>
2021-06-20 11:23:20 +1000

    CHG: destroyPlayer()
    Important: after calling changePlayer(), we must set the new game's currentPlayer property to our own currentPlayer property, so that whoever died gets the first shot.

--- ExplodingMonkeys/GameScene.swift
+++ ExplodingMonkeys/GameScene.swift
@@ -144,5 +144,40 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 			destroy(player: player2)
 		}
 	}
+
+		func destroy(player: SKNode) {
+			if let explosion = SKEmitterNode(fileNamed: "hitPlayer") {
+				explosion.position = player.position
+				addChild(explosion)
+			}
+
+			player.removeFromParent()
+			banana.removeFromParent()
+
+			DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+				let newGame = GameScene(size: self.size)
+				newGame.viewController = self.viewController
+				self.viewController.currentGame = newGame
+
+				self.changePlayer()
+				newGame.currentPlayer = self.currentPlayer
+
+				let transition = SKTransition.doorway(withDuration: 1.5)
+				self.view?.presentScene(newGame, transition: transition)
+			}
+		}
+
+	func changePlayer() {
+		if currentPlayer == 1 {
+			currentPlayer = 2
+		} else {
+			currentPlayer = 1
+		}
+
+		viewController.activatePlayer(number: currentPlayer)
+
+	}
+
+
 	
 }
